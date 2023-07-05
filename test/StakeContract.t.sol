bfa0f7a31e93715177d7030834636aa10198c7c4
Luke Sheppard <sheppiegit@gmail.com>
2023-07-05 17:48:51 -0400

    Added a test contract for the StakeContractTest.

---
+++ test/StakeContract.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+import "forge-std/Test.sol";
+
+contract StakeContractTest is Test {
+    function setUp() public {}
+
+    function testExample() public {}
+}
