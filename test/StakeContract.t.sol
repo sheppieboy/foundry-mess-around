e79702c01d5d434861be5674bacf52530c2e6e07
Luke Sheppard <sheppiegit@gmail.com>
2023-07-07 15:44:13 -0400

    Added a mock ERC20 for test pruposes.

--- test/StakeContract.t.sol
+++ test/StakeContract.t.sol
@@ -1,9 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 import "forge-std/Test.sol";
+import "../src/Stake.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract StakeContractTest is Test {
-    function setUp() public {}
+    StakeContract private stakeContract;
+
+    function setUp() public {
+        stakeContract = new StakeContract();
+    }
 
     function testExample() public {}
+}
+
+contract mockERC20 is ERC20 {
+    constructor() ERC20("MOCK", "MK20") {
+        _mint(msg.sender, 1000e18);
+    }
 }
