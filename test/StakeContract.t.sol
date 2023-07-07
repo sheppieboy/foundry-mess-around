739c4484238cac7aed06f8311591a3eb20465fa7
Luke Sheppard <sheppiegit@gmail.com>
2023-07-07 15:48:54 -0400

    basic test to see if stake returns a true boolean using a mock ERC20.

--- test/StakeContract.t.sol
+++ test/StakeContract.t.sol
@@ -6,15 +6,22 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract StakeContractTest is Test {
     StakeContract private stakeContract;
+    MockERC20 private mockERC20;
 
     function setUp() public {
         stakeContract = new StakeContract();
+        mockERC20 = new MockERC20();
     }
 
-    function testExample() public {}
+    function testExample() public {
+        uint256 amount = 10e18;
+        mockERC20.approve(address(stakeContract), amount);
+        bool stakeWorked = stakeContract.stake(amount, address(mockERC20));
+        assertTrue(stakeWorked);
+    }
 }
 
-contract mockERC20 is ERC20 {
+contract MockERC20 is ERC20 {
     constructor() ERC20("MOCK", "MK20") {
         _mint(msg.sender, 1000e18);
     }
